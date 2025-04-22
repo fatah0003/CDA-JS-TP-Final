36df06c3af2a9530ac396eecfbb6e86176951b54
fatah0003 <fatahutd@gmail.com>
2025-04-22 15:28:33 +0200

    affichage visuel

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 const words = [
-    "pomme",
-    "livre",
-    "table",
-    "chien",
-    "glace",
-    "plage",
-    "rouge",
-    "vertu",
-    "brume",
-    "fleur"
+    "pomme", "livre", "table", "chien", "glace", "plage", "rouge", "vertu", "brume", "fleur",
+    "arbre", "cerise", "blaze", "roche", "salut", "porte", "peine", "valet", "nuit", "foyer",
+    "saison", "farce", "panne", "frais", "chose", "flore", "vague", "tigre", "pince", "sable"
 ];
 
+
+
 const word = words[Math.floor(Math.random() * words.length)];
-console.log("Mot à deviner :", word); // à cacher plus tard
+// console.log("Mot à deviner :", word); //
 
 let nmbTry = 5;
 let lettersPlayer = [];
 let gameOver = false;
+
+const grille = document.getElementById("grille");
+
+for (let i = 0; i < 25; i++) {
+    const caseLettre = document.createElement("div");
+    caseLettre.classList.add("case");
+    grille.appendChild(caseLettre);
+}
+let ligneActuelle = 0
 
 addEventListener("keydown", (event) => {
     if (gameOver) return; // ⛔ empêche d’écrire après la fin
@@ -26,8 +30,22 @@ addEventListener("keydown", (event) => {
     if (/^[a-z]$/.test(key) && lettersPlayer.length < 5) {
         lettersPlayer.push(key);
         console.log("Lettres saisies :", lettersPlayer.join(""));
+        const caseKey = document.querySelectorAll('.case')
+        let x = ligneActuelle * 5 + lettersPlayer.length - 1
+        
+        caseKey[x].innerText = key
     }
-
+    if (event.key === "Backspace") {
+        if (lettersPlayer.length > 0) {
+            lettersPlayer.pop();
+    
+            const caseKey = document.querySelectorAll('.case');
+            let x = ligneActuelle * 5 + lettersPlayer.length; 
+    
+            caseKey[x].innerText = ""; 
+        }
+    }
+    
     if (event.key === "Enter") {
         if (lettersPlayer.length !== 5) {
             console.log("⛔ Il faut entrer 5 lettres avant de valider !");
@@ -48,8 +66,23 @@ addEventListener("keydown", (event) => {
                 gameOver = true; // 🛑 bloque aussi à la fin du jeu
             }
         }
-
+        const caseKey = document.querySelectorAll('.case')
+        for (let i = 0; i< word.length; i++){
+            if(lettersPlayer[i] === word[i]){
+                let x = ligneActuelle * 5 + i;
+                caseKey[x].classList.add('case_valide');
+            }
+            
+            else if(word.includes(lettersPlayer[i])){
+                let x = ligneActuelle * 5 + i;
+                caseKey[x].classList.add('case_place');
+            }
+        }
         lettersPlayer = [];
+        ligneActuelle++
+        
     }
 });
+
+
 
